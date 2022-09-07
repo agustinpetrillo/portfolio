c5f0e5ded4b6ab7ec2ecf5fd072b4e6d78f5a2dd
Agustin Petrillo <agustinpetrillo1@gmail.com>
2022-09-07 18:19:53 -0300

    fixed some bugs

--- components/FifthSection.jsx
+++ components/FifthSection.jsx
@@ -165,7 +165,7 @@ const FifthSection = () => {
           </div>
         </Container>
       </Background>
-      <style jsx>{`
+      <style>{`
         input::-webkit-outer-spin-button,
         input::-webkit-inner-spin-button {
           -webkit-appearance: none;

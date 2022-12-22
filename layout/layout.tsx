f8858ea9d8c009af79883adcd35b19ad7f71dcba
Agustin Petrillo <agustinpetrillo1@gmail.com>
2022-12-22 17:17:16 -0300

    remove [] from children props

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 // import Footer from "../components/Footer";
 
 type Props = {
-  children: JSX.Element | JSX.Element[];
+  children: JSX.Element;
 };
 
 const Layout = ({ children }: Props) => {

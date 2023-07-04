90917a489e1d01f237e538160eb32a9731303149
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-07-04 16:12:21 -0300

    added style

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,11 +2,12 @@ type Props = {
   children: JSX.Element | JSX.Element[];
   className?: string;
   id?: string;
+  style?: React.CSSProperties;
 };
 
-const Background = ({ children, className, id }: Props) => {
+const Background = ({ children, className, id, style }: Props) => {
   return (
-    <div id={id} className={`w-full ${className}`}>
+    <div id={id} className={`w-full ${className}`} style={style}>
       {children}
     </div>
   );

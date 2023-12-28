4dd166ae02bd9c1cf29b9d39ad9450b49e5cd284
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-12-28 17:17:28 -0300

    background in light and dark mode

--- components/SpaceBackground.tsx
+++ components/SpaceBackground.tsx
@@ -1,11 +1,16 @@
+import { useTheme } from "next-themes";
+
 export default function SpaceBackground() {
+  const { theme } = useTheme();
   return (
     <video
       src="/assets/video/space.mp4"
       autoPlay
       loop
       muted
-      className="fixed top-0 left-0 object-cover w-full h-screen opacity-25 -z-50"
+      className={`fixed top-0 left-0 object-cover w-full h-screen opacity-25 -z-50 ${
+        theme === "light" && "invert"
+      }`}
     />
   );
 }
